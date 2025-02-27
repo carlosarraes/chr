2006242a5cc2cea3eb242d1e3d48695cd90c4c57
carraes <carraeshb@gmail.com>
2025-02-26 22:34:25 -0300

    feat: beggining of pick impl

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,12 @@ struct Cli {
 
 #[derive(Parser)]
 struct PickArgs {
-    #[arg(short, long, default_value = "5")]
+    #[arg(short, long, default_value = "5", help = "Number of commits to pick")]
     count: u32,
+    #[arg(short, long, help = "Pick only your commits")]
+    me: bool,
+    #[arg(short, long, help = "Show commits instead of picking")]
+    show: bool,
 }
 
 #[derive(Subcommand)]
@@ -48,38 +52,85 @@ fn main() {
 }
 
 fn pick(args: PickArgs) {
-    let mut git = Command::new("git");
-
-    let branch_name = git
+    let branch_output = Command::new("git")
         .arg("branch")
         .arg("--show-current")
         .output()
         .expect("Failed to get current branch name")
         .stdout;
-
-    let branch_name = String::from_utf8(branch_name).unwrap();
-
+    let branch_name = String::from_utf8(branch_output).unwrap();
     let branch_name = branch_name.trim();
-
-    let branch_name = branch_name.split("-").collect::<Vec<&str>>();
-
-    let card_number = branch_name[1];
+    let parts = branch_name.split("-").collect::<Vec<&str>>();
+    let card_number = parts[1];
 
     let hml_branch = format!("{}{}{}", PREFIX, card_number, SUFFIX_HML);
     let prd_branch = format!("{}{}{}", PREFIX, card_number, SUFFIX_PRD);
 
-    let output = git
+    let log_output = Command::new("git")
         .arg("log")
         .arg(format!("^{}", hml_branch))
         .arg(prd_branch)
-        .arg(args.count.to_string())
+        .arg(format!("-{}", args.count))
         .arg("--format=%h %C(green)%an%C(reset) %s")
+        .arg("--color=always")
         .output()
         .expect("Failed to execute git log");
 
-    let output = String::from_utf8(output.stdout).unwrap();
+    let output = String::from_utf8(log_output.stdout).unwrap();
 
-    println!("{}", output);
+    if args.me {
+        let user_output = Command::new("git")
+            .arg("config")
+            .arg("user.name")
+            .output()
+            .expect("Failed to get git user name");
+        let username = String::from_utf8(user_output.stdout)
+            .unwrap()
+            .trim()
+            .to_string();
+
+        for line in output.lines() {
+            if line.contains(&username) {
+                println!("{}", line);
+            } else {
+                break;
+            }
+        }
+    } else {
+        print!("{}", output);
+    }
+
+    if !args.show {
+        let ques = dialoguer::Confirm::new()
+            .with_prompt("Do you want to cherry-pick these commits?")
+            .interact()
+            .unwrap();
+
+        if ques {
+            let commit_hashes = output
+                .lines()
+                .map(|line| line.split_whitespace().collect::<Vec<&str>>()[0])
+                .collect::<Vec<&str>>();
+
+            let rev_output = Command::new("git")
+                .arg("rev-list")
+                .arg("--reverse")
+                .arg(format!(
+                    "{}^..{}",
+                    commit_hashes[0],
+                    commit_hashes[commit_hashes.len() - 1]
+                ))
+                .spawn()
+                .expect("Failed to execute git rev-list");
+
+            Command::new("git")
+                .arg("cherry-pick")
+                .arg("--stdin")
+                .stdin(rev_output.stdout.unwrap())
+                .status()
+                .expect("Failed to execute git cherry-pick");
+        }
+    }
 }
 
 fn start() {
